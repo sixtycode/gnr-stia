758ed6117efe3d0448a28264b9126d68d69a60f6
sixtycode <sagaraperkasa.work@gmail.com>
2026-04-02 03:17:59 +0700

    feat: website title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "GENERALIS";
+export const SITE_DESCRIPTION = "Media STIA YPPT Priatim Tasikmalaya";
